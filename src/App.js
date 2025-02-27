3aa0c3a47032fb8270b22398d005586c320fa221
Xori <xori93@aol.com>
2025-02-26 22:37:02 -0500

    books page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Footer from './components/Footer';
 import Nav from './components/Nav';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Books from './pages/Books';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route>
           <Nav />
-          <Home />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" component={Books} />
           <Footer />
-        </Route>
       </div>
     </Router>
   );
