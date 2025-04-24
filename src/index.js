52557862ca7c44310188f5f005f08fa79e4354b6
Xori <xori93@aol.com>
2025-04-24 18:35:21 -0400

    error

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faSpinner, faArrowLeft, faShoppingCart, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { } from '@fortawesome/free-brands-svg-icons'
+import { faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faSpinner, faArrowLeft, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faSpinner, faArrowLeft, faShoppingCart, faArrowLeft);
+library.add(faBars, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faSpinner, faArrowLeft, faShoppingCart)
 
-const root = createRoot(document.getElementById('root')); // Create a root
-root.render(
+ReactDOM.render(
+  <>
     <App />
-
+  </>,
+  document.getElementById('root')
 );
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
